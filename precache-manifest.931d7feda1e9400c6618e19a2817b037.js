6387fbd9528127472db2fe939a417198f2efdbd2
Wises Keshom <wises.keshom@gmail.com>
2020-06-12 13:36:07 +0700

    Updates

---
+++ precache-manifest.931d7feda1e9400c6618e19a2817b037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e442b823228fed5654ffd85109a420d0",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "0bf943dcea45599a71ce",
+    "url": "/simple-todo-app/static/css/main.d3faecd3.chunk.css"
+  },
+  {
+    "revision": "0139ce81d585db9a214e",
+    "url": "/simple-todo-app/static/js/2.b9476618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app/static/js/2.b9476618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bf943dcea45599a71ce",
+    "url": "/simple-todo-app/static/js/main.4cf31ff2.chunk.js"
+  },
+  {
+    "revision": "e165d461a30ce2094b0e",
+    "url": "/simple-todo-app/static/js/runtime-main.065a362d.js"
+  }
+]);
