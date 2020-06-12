4886645b947d0ad9e60eebbfb96f30e2498138c4
Wises Keshom <wises.keshom@gmail.com>
2020-06-12 15:30:41 +0700

    third commit: before convert to React Hooks

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const headerStyle = {
-  padding: '20px 0',
-  lineHeight: '2em'
-}
+// const Header = () => {
+//   const headerStyle = {
+//     padding: '20px 0',
+//     lineHeight: '2em'
+//   }
 
-const Header = () => {
-  return (
-    <header style={headerStyle}>
-      <h1 style={{ fontSize: '25px', marginBottom: '15px' }}>Simple Todo App</h1>
-      <p style={{ fontSize: '19px' }}>Please add to-dos item(s) through the input field</p>
-    </header>
-  )
+//   return (
+//     <header style={headerStyle}>
+//       <h1 style={{ fontSize: '25px', marginBottom: '15px' }}>Simple Todo App</h1>
+//       <p style={{ fontSize: '19px' }}>Please add to-dos item(s) through the input field</p>
+//     </header>
+//   )
+// }
+
+class Header extends Component {
+  componentDidUpdate (prevProps, prevState) {
+    if (prevProps.headerSpan !== this.props.headerSpan) {
+      const x = Math.floor(Math.random() * 256)
+      const y = Math.floor(Math.random() * 256)
+      const z = Math.floor(Math.random() * 256)
+      const bgColor = 'rgb(' + x + ',' + y + ',' + z + ')'
+
+      if (prevProps.headerSpan !== this.props.headerSpan) {
+        document.getElementById('inH1').innerHTML = 'clicked'
+        document.getElementById('inH1').style.backgroundColor = bgColor
+      }
+    }
+  }
+
+  render () {
+    const headerStyle = {
+      padding: '20px 0',
+      lineHeight: '2em'
+    }
+    return (
+      <header style={headerStyle}>
+        <h1 style={{ fontSize: '25px', marginBottom: '15px' }}>
+          Simple Todo App <span id='inH1' />
+        </h1>
+        <p style={{ fontSize: '19px' }}>
+          Please add to-dos item(s) through the input field
+        </p>
+      </header>
+    )
+  }
 }
 
 export default Header
--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,26 +3,12 @@ import TodosList from './TodosList'
 import Header from './Header'
 import InputTodo from './InputTodo'
 import { v4 as uuidv4 } from 'uuid'
+import axios from 'axios'
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: 'Setup development environment',
-        completed: true
-      },
-      {
-        id: uuidv4(),
-        title: 'Develop website and add content',
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: 'Deploy to live server',
-        completed: false
-      }
-    ]
+    todos: [],
+    show: false
   };
 
   handleChange = (id) => {
@@ -32,35 +18,56 @@ class TodoContainer extends React.Component {
           todo.completed = !todo.completed
         }
         return todo
-      })
+      }),
+      show: !this.state.show
     })
   };
 
   handleDelTodo = (id) => {
-    this.setState({
-      todos: [
-        ...this.state.todos.filter(todo => {
-          return todo.id !== id
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(reponse =>
+        this.setState({
+          todos: [
+            ...this.state.todos.filter(todo => { return todo.id !== id })
+          ]
         })
-      ]
-    })
+      )
   };
 
   addTodoItem = title => {
-    const newTodo = {
-      id: uuidv4(),
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
       title: title,
       completed: false
-    }
-    this.setState({
-      todos: [...this.state.todos, newTodo]
+    }).then(
+      response => {
+        const data = response.data
+        data.id = uuidv4()
+
+        this.setState({
+          todos: [...this.state.todos, data]
+        })
+      }
+    )
+  };
+
+  componentDidMount () {
+    axios.get('https://jsonplaceholder.typicode.com/todos', {
+      params: {
+        _limit: 10
+      }
+    }).then(response => {
+      const modifiedData = response.data.map(data => {
+        data.id = uuidv4()
+        return data
+      })
+      this.setState({ todos: modifiedData })
     })
   };
 
   render () {
     return (
       <div className='container'>
-        <Header />
+        <Header headerSpan={this.state.show} />
         <InputTodo addTodoItem={this.addTodoItem} />
         <TodosList
           todos={this.state.todos}
